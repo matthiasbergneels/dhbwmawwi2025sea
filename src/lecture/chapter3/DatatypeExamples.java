d3c86bb83a84dbc7e3eb24a4636ac19624f9e646
Matthias Berg-Neels <matthias.berg-neels@sap.com>
2025-10-20 19:52:35 +0200

    integer literal examples

--- src/lecture/chapter3/DatatypeExamples.java
+++ src/lecture/chapter3/DatatypeExamples.java
@@ -29,5 +29,31 @@ public class DatatypeExamples {
     // Declare and initialize a character constant
     final char MY_CONSTANT_CHAR = '!';
 
+    // integer literals
+    myInt = 10; // --> value 10
+    System.out.println("Decimal 10: " + myInt);
+    myInt = 0157;
+    System.out.println("Octal 10: " + myInt);
+    myInt = 0x10EB;
+    System.out.println("Hex 10: " + myInt);
+    myInt = 0b10;
+
+    System.out.println("Binär 10: " + myInt);
+
+    final int COLOR_RED = 0xFF0000;
+    System.out.println("Color Red: " + COLOR_RED);
+    final int COLOR_GREEN = 0x00FF00;
+    final int COLOR_BLUE = 0x0000FF;
+
+    System.out.println("Farbe Rot binary: " + Integer.toBinaryString(COLOR_RED));
+
+    long myFirstLongVariable = 2_240_000_000l;
+    myFirstLongVariable = 3L;
+
+    byte myFirstShortVariable = 5;
+    // myFirstShortVariable = 128; --> value to big for byte
+
+    
+
   }
 }
